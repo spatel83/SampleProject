68f6c7ec6772aa511f0df136496a9262f38e948a
spatel83 <spatel.gvc@gmail.com>
2024-12-07 13:00:19 +0530

    code Changes

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript functionality
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Website Loaded");
+    // Add any JS functionality here if needed in the future
+});
